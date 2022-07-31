9abd14ff981ef0f05beb3fc6be087b9be715f694
mikezzb <mikezhoudev@gmail.com>
2022-08-01 00:06:50 +0800

    Add base class

--- src/AsciiEffect.ts
+++
@@ -1,7 +0,0 @@
-export default class AsciiEffect {
-  private input;
-  constructor(input) {
-    this.input = input;
-    console.log(this.input);
-  }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-import AsciiEffect from './AsciiEffect';
+export type AEConfig = {
+  output?: HTMLCanvasElement;
+};
 
-export default AsciiEffect;
+type Size = {
+  width: number;
+  height: number;
+};
+
+export default class AsciiEffect {
+  private input: HTMLVideoElement;
+  private canvas: HTMLCanvasElement;
+  private config: AEConfig;
+  private ready: boolean;
+  private size: Size | undefined;
+  constructor(input: HTMLVideoElement, config?: AEConfig) {
+    this.input = input;
+    this.config = config || {};
+    this.canvas = this.config.output || document.createElement('canvas');
+    this.ready = false;
+    this.input.oncanplay = () => {
+      console.log('ready');
+      this.ready = true;
+      this.size = {
+        width: this.input.width,
+        height: this.input.height,
+      };
+    };
+    console.log('Constructed');
+    this.animate = this.animate.bind(this);
+    requestAnimationFrame(this.animate);
+  }
+  animate() {
+    requestAnimationFrame(this.animate);
+    if (this.ready) this.render();
+  }
+  render() {}
+}
