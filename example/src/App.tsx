969c7c0d47e69897538738b918c902465a5263a5
mikezzb <mikezhoudev@gmail.com>
2022-12-25 16:24:13 +0800

    demo: ascii text container

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,30 +1,49 @@
-import AsciiEffect from "my-package";
-import { useRef } from "react";
-import Webcam from "react-webcam";
+import AsciiEffect from 'ascii-video-effect';
+import { useRef } from 'react';
+import Webcam from 'react-webcam';
 import './App.scss';
 
 const SIZE = {
-  width: 480,
-  height: 640,
-}
+  width: 640,
+  height: 480,
+};
 
 function App() {
   const effectRef = useRef<AsciiEffect>();
   const webcamRef = useRef<Webcam>(null);
+  const textRef = useRef<HTMLPreElement | null>(null);
+  const onResultAvaiable = (asciiStr: string) => {
+    if (textRef.current) {
+      textRef.current.innerHTML = asciiStr;
+    }
+  };
+  const onVideoReady = () => {
+    const effect = new AsciiEffect(
+      webcamRef?.current?.video as HTMLVideoElement,
+      onResultAvaiable,
+      SIZE.width,
+      SIZE.height
+    );
+    effectRef.current = effect;
+    if (textRef.current) {
+      const styles = effect.getAsciiContainerStyles();
+      Object.entries(styles).forEach(([k, v]) => {
+        (textRef.current as any).style[k as any] = v;
+      });
+    }
+  };
   return (
     <div className="App">
-      <div className="effect-container">
+      <div style={{ display: 'flex' }} className="effect-container">
         <Webcam
           audio={false}
+          style={{ visibility: 'hidden', maxWidth: 0, maxHeight: 0 }}
           height={SIZE.height}
-          ref={webcamRef}
           width={SIZE.width}
-          onCanPlay={() => {
-            effectRef.current = new AsciiEffect(
-              webcamRef?.current?.video as HTMLVideoElement
-            )
-          }}
+          ref={webcamRef}
+          onCanPlay={onVideoReady}
         />
+        <pre className="ascii-renderer" ref={ref => (textRef.current = ref)} />
       </div>
     </div>
   );
